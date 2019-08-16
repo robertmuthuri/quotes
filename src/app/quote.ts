cbaca07131bad3d4d7936e0cacd778baeeb0225f
Robert Muthuri <muthuri.r@gmail.com>
2019-08-16 17:28:41 +0300

    Changes Quote blueprint class by using a constructor function.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,13 @@
 export class Quote {
-  id: number;
-  name: string;
-  quoteText: string;
-  quoteAuthor: string;
-  quoteSubmitter: string;
-  quoteUpVote: number = 0;
-  quoteDownVote: number = 0;
+  // tslint:disable-next-line:max-line-length
+
+showQuote: boolean;
+
+  // tslint:disable-next-line:max-line-length
+  constructor(public id: number, public name: string, public quoteText: string, public quoteAuthor: string, public quoteSubmitter: string, public quoteUpVote: number = 0, public quoteDownVote: number = 0) {
+
+    this.showQuote = false;
+
+  }
+
 }
