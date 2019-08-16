aeb8d18dd5967cefbd3c5574b36d6c713f1b4b6b
Robert Muthuri <muthuri.r@gmail.com>
2019-08-16 17:01:11 +0300

    Adds class logic definition to quote.ts.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,9 @@
 export class Quote {
+  id: number;
+  name: string;
+  quoteText: string;
+  quoteAuthor: string;
+  quoteSubmitter: string;
+  quoteUpVote: number = 0;
+  quoteDownVote: number = 0;
 }
