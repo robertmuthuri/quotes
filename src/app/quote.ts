ebe6d4ee934637fa92c14d442704c154ea1e4471
Robert Muthuri <muthuri.r@gmail.com>
2019-08-17 22:56:32 +0300

    Displays data received from the quote property bound in parent component to the quote detail component.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,13 +1,12 @@
 export class Quote {
   // tslint:disable-next-line:max-line-length
 
-showQuote: boolean;
+  // showQuote: boolean;
 
-  // tslint:disable-next-line:max-line-length
   constructor(public id: number, public name: string, public quoteText: string, public quoteAuthor: string, public quoteSubmitter: string) {
   // public quoteUpVote: number = 0, public quoteDownVote: number = 0
 
-    this.showQuote = false;
+    // this.showQuote = false;
 
   }
 
