3aa476692abbcc9e5518c32f738ee70c4ba3c7aa
Robert Muthuri <muthuri.r@gmail.com>
2019-08-18 00:01:17 +0300

    Adds a boolean showQuote property to the Quote class definition and initializes it to false.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,12 +1,12 @@
 export class Quote {
   // tslint:disable-next-line:max-line-length
 
-  // showQuote: boolean;
+  showQuote: boolean;
 
   constructor(public id: number, public name: string, public quoteText: string, public quoteAuthor: string, public quoteSubmitter: string) {
   // public quoteUpVote: number = 0, public quoteDownVote: number = 0
 
-    // this.showQuote = false;
+    this.showQuote = false;
 
   }
 
