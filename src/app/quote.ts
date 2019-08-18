104af0fea0a73198f0e87292614d0970e5797a18
Robert Muthuri <muthuri.r@gmail.com>
2019-08-18 17:39:40 +0300

    Adds upvote and downvote properties to the class constructor in quote.ts.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,8 +3,8 @@ export class Quote {
 
   showQuote: boolean;
 
-  constructor(public id: number, public name: string, public quoteText: string, public quoteAuthor: string, public quoteSubmitter: string) {
-  // public quoteUpVote: number = 0, public quoteDownVote: number = 0
+  // tslint:disable-next-line:max-line-length
+  constructor(public id: number, public name: string, public quoteText: string, public quoteAuthor: string, public quoteSubmitter: string, public quoteUpVote: number = 0, public quoteDownVote: number = 0) {
 
     this.showQuote = false;
 
