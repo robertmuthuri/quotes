4e573c0a7de7808103cb57a7d2300db5b3bee018
Robert Muthuri <muthuri.r@gmail.com>
2019-08-18 17:42:43 +0300

    Generates a voting component for upvotes and downvotes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { VotingComponent } from './voting/voting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    VotingComponent
   ],
   imports: [
     BrowserModule,
