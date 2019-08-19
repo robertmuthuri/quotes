d69c6c7ba4111458866e6f49eab2f6ed26d1f3cf
Robert Muthuri <muthuri.r@gmail.com>
2019-08-19 08:04:33 +0300

    Adds tranform logic in the date-count pipe.

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
-  }
+  transform(value: any): number {
 
+    let today: Date = new Date(); // Get current date and time.
+
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+
+    var dateDifference = Math.abs(value - todayWithNoTime);
+
+    const secondsInDay = 86400 // 60 seconds * 60 minutes in an hour * 24 hours in a day
+
+    var dateDifferenceSeconds = dateDifference * 0.001;
+
+    var dateCounter = dateDifferenceSeconds / secondsInDay;
+
+    if (dateCounter >= 1 && value > todayWithNoTime) {
+      return dateCounter
+    } else {
+      return null;
+    }
+  }
 }
